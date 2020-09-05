f24818f5d2aec3fdf13f0e042fc3c464d796c9db
Trezentos <gustavofagundes1998@hotmail.com>
2020-09-05 15:58:49 -0300

    The site is done!

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const navLink = document.querySelector('#nav')
+const nav = document.querySelector('#nav')
+const burger = document.querySelector("#nav .burger")
+const navLink = document.querySelector('#nav .nav-links')
 const owlSiteLogo = document.querySelector('#nav a')
 const spanOwlSitelogo = document.querySelector('#nav .logo a span')
 const span2OwlSitelogo = document.querySelector('#nav .logo a span:nth-child(2)')
@@ -6,17 +8,19 @@ const span2OwlSitelogo = document.querySelector('#nav .logo a span:nth-child(2)'
 document.addEventListener('scroll', ()=>{
     let scroll = window.scrollY;
 
-    if(scroll>450){  
-        navLink.style.backgroundColor = "#181818"
-        owlSiteLogo.style.color = "white"
-        span2OwlSitelogo.style.color = spanOwlSitelogo.style.color = 'rgb(173, 141, 80)'
-        
-        
-        
-    }else{
-        navLink.style.backgroundColor = "transparent"
-        spanOwlSitelogo.style.color = owlSiteLogo.style.color = span2OwlSitelogo.style.color = "transparent"
-    }
+        if(scroll>450 ){  
+            nav.style.backgroundColor = "#181818"
+            owlSiteLogo.style.color = "white"
+            span2OwlSitelogo.style.color = spanOwlSitelogo.style.color = 'rgb(173, 141, 80)'
+                  
+        }else{
+            nav.style.backgroundColor = "transparent"
+            spanOwlSitelogo.style.color = owlSiteLogo.style.color = span2OwlSitelogo.style.color = "transparent"
+        }
+})
 
+burger.addEventListener('click', ()=>{
+    
+    navLink.classList.toggle('nav-links-active')
 
 })
